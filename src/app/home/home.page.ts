7a6e1ab8907ea01e54705e8100c14d1bc6191573
kaifyyz <111551038+kaifyyz@users.noreply.github.com>
2026-03-14 06:58:20 +0000

    feat: Добавил курсы и вывел в таблицы с кнопкой

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
 
+interface Kurs{
+  telegramname: string
+  authorname: string
+  price: number
+}
+
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -10,4 +17,16 @@ export class HomePage {
 
   constructor() {}
   myname: string = "Work4App"
+  sport: string[] = [
+    "Анжумания",
+    "Бегит",
+    "Пресс качат"
+  ]
+  books: Kurs[] = [
+    {telegramname: "Курсы для чайников", authorname: "Смаев", price: 100000},
+    {telegramname: "Блиновская для спортсменов", authorname: "Блиновская", price: 9795000}
+  ]
+  btnClick(){
+    this.myname = this.myname + "|"
+  }
 }
