b5b97dcf5993e2ef8808d31e2d7596418235dad4
kaifyyz <111551038+kaifyyz@users.noreply.github.com>
2026-04-11 12:00:07 +0000

    Добавили возможность создания курсов и их отображения в виде таблицы, а также удаление курсов по индексу.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -17,16 +17,36 @@ export class HomePage {
 
   constructor() {}
   myname: string = "Work4App"
-  sport: string[] = [
-    "Анжумания",
-    "Бегит",
-    "Пресс качат"
-  ]
-  books: Kurs[] = [
-    {telegramname: "Курсы для чайников", authorname: "Смаев", price: 100000},
-    {telegramname: "Блиновская для спортсменов", authorname: "Блиновская", price: 9795000}
-  ]
+  sport: string[] = []
+  books: Kurs[] = []
+  
+  // форма для добавления нового курсв
+  newTelegramName: string = '';
+  newAuthorName: string = '';
+  newPrice: number = 0;
+  
   btnClick(){
     this.myname = this.myname + "|"
+  }
+  
+  // добавить новый курс
+  addBook() {
+    if (this.newTelegramName.trim() && this.newAuthorName.trim() && this.newPrice > 0) {
+      const newBook: Kurs = {
+        telegramname: this.newTelegramName,
+        authorname: this.newAuthorName,
+        price: this.newPrice
+      };
+      this.books.push(newBook);
+      
+      //очистить форму
+      this.newTelegramName = '';
+      this.newAuthorName = '';
+      this.newPrice = 0;
+    }
+  }
+  
+  deleteBook(index: number) {
+    this.books.splice(index, 1);
   }
 }
